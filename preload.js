5d3c91f4c7cefbbd9ab8688aa235accfb547dfb0
Bloxview <adensevenanderson7@gmail.com>
2026-02-07 19:42:53 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electron', {
-    getConfig: () => ipcRenderer.invoke('get-config'),
-    saveConfig: (config) => ipcRenderer.invoke('save-config', config),
-    installModule: (name, code) => ipcRenderer.invoke('install-module', name, code),
-    getFlashUpdates: () => ipcRenderer.invoke('get-flash-updates'),
-    executeFlashUpdate: (fileName) => ipcRenderer.invoke('execute-flash-update', fileName),
-    onFlashUpdate: (callback) => ipcRenderer.on('flash-update', (event, moduleName) => callback(moduleName))
+contextBridge.exposeInMainWorld('electronAPI', {
+  getConfig: () => ipcRenderer.invoke('get-config'),
+  saveConfig: (config) => ipcRenderer.invoke('save-config', config),
+  onFlashUpdate: (callback) => {
+    ipcRenderer.on('flash-update', (event, moduleName) => callback(moduleName));
+    // Return cleanup function
+    return () => ipcRenderer.removeAllListeners('flash-update');
+  },
+  isDevelopment: process.env.NODE_ENV === 'development'
 });
